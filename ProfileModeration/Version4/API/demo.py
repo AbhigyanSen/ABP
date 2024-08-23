382f098c5a290ae6e2eb6b8ae081ec03ba6f4b2a
AbhigyanSen <abhigyansen16@gmail.com>
2024-08-23 12:20:37 +0530

    updated v4

--- ProfileModeration/Version4/API/demo.py
+++ ProfileModeration/Version4/API/demo.py
@@ -17,8 +17,8 @@ from PIL import Image
 from io import BytesIO
 from transformers import AutoModelForImageClassification, ViTImageProcessor
 
-BASE_FOLDER = "/home/abp/Documents/ABPProduction/ABP/Version4/Images"
-YOLO_FOLDER = "/home/abp/Documents/ABPProduction/ABP/Version4/best.pt"
+BASE_FOLDER = "/home/abp/Documents/ABPProduction/ABP/ProfileModeration/Version4/Images"
+YOLO_FOLDER = "/home/abp/Documents/ABPProduction/ABP/ProfileModeration/Version4/best.pt"
 
 
 try:
